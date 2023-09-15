830beca0713255aeb819ea794d8f9738c5266e57
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-09-15 13:38:25 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -32,6 +32,26 @@
         for ( int i = 0; i < m; ++i )
         {
             for (int j = 0; j < n; j++)
+                if (mat[i, j] == x)
+                {
+                    Console.WriteLine("Posição " + i + "," + j + ":");
+                    if (j > 0)
+                    {
+                        Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                    }
+                    if (i > 0)
+                    {
+                        Console.WriteLine("Acima: " + mat[i -1, j]);
+                    }
+                    if (j < n - 1)
+                    {
+                        Console.WriteLine("Direita: " + mat[i , j + 1]);
+                    }
+                    if (i < n - 1)
+                    {
+                        Console.WriteLine("Abaixo: " + mat[i +1 , j]);
+                    }
+                }
         }
     }
 }
